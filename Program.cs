5eca27acd32b6140be9ebb03af6bd6d1e954fabe
MesuDevastator <administrator@laptismc.xyz>
2022-12-10 19:24:36 +0800

    添加项目文件。

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using Serilog;
+using Serilog.Core;
+
+namespace Fyson;
+
+public static class Program
+{
+    private static Logger _logger = new LoggerConfiguration()
+        .WriteTo.Console()
+        .CreateLogger();
+
+    public static void Main()
+    {
+        
+    }
+}
